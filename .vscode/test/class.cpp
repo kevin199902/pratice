2aa3ecab0d69acedeb1aa8b14aece382a8cf4e2d
YiChen-Li223 <zxc2983439@gmail.com>
2024-09-23 22:59:45 +0800

    add class.cpp

---
+++ .vscode/test/class.cpp
@@ -0,0 +1,27 @@
+#include <iostream>  
+
+class T {  
+  int val;
+
+public:
+//初始化法(一)
+  T():val{1}{
+    std::cout << "Initialization successful\n";
+  }
+
+// 初始化法(二)
+// T(){
+//   val = 10;
+// }
+
+  void set_other_val(T &other) { other.val = 100; };  
+  int get_val() { return val; }  
+};  
+
+int main()  
+{  
+  T t1, t2;  
+  t1.set_other_val(t2);  
+  std::cout << t2.get_val() << "\n";  
+  std::cout << t1.get_val() << "\n";
+}  
