91236b9de7c08f15f588d997ff477b1bd4a30de7
YiChen-Li223 <zxc2983439@gmail.com>
2024-09-25 18:00:05 +0800

    add class T3

--- .vscode/structor/Initialization_order.cpp
+++ .vscode/structor/Initialization_order.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 class A {
-public:
-    A() { cout << "A() called\n"; }
-    A(int x) { cout << "A(int) called with " << x << "\n"; }
+    public:
+        A() { cout << "A() called\n"; }
+        A(int x) { cout << "A(int) called with " << x << "\n"; }
 };
 
 class T1 {
@@ -15,13 +15,17 @@ class T1 {
 class T2 {
     A a;  // 未初始化，需透過建構子初始化
     A b;
-public:
-    T2() : a{}, b{4} {}  // 成員初始化列表
-    //T2() : b{4}{}
+    public:
+        T2() : a{}, b{4} {}  // 成員初始化列表
+};
+
+class T3{
+    T3(){};
 };
 
 int main() {
     T1 t1;  // A() called
     T2 t2;  // A() called
+    T3 t3;
     return 0;
 }
