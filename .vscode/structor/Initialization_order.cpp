4425128ae75c77410e9d4da30e384fa873885d96
YiChen-Li223 <zxc2983439@gmail.com>
2024-09-25 18:27:58 +0800

    deleate T3 and change T2

--- .vscode/structor/Initialization_order.cpp
+++ .vscode/structor/Initialization_order.cpp
@@ -2,30 +2,35 @@
 using namespace std;
 
 class A {
-    public:
-        A() { cout << "A() called\n"; }
-        A(int x) { cout << "A(int) called with " << x << "\n"; }
+public:
+    A() { cout << "A() called\n"; }
+    A(int x) { cout << "A(int) called with " << x << "\n"; }
 };
 
 class T1 {
-    A a{};  // 直接成員初始化
-    A b{3};
+    A a;  
+    A b;
+public:
+    T1() : a(), b(4) {} // T1 建構子：初始化 a 與 b
 };
 
 class T2 {
-    A a;  // 未初始化，需透過建構子初始化
-    A b;
-    public:
-        T2() : a{}, b{4} {}  // 成員初始化列表
-};
+    int value;
+public:
+    // 無參數建構子
+    T2() {
+        cout << "T2() called\n";
+    }
 
-class T3{
-    T3(){};
+    // 接受一個參數的建構子
+    T2(int val) : value(val) {
+        cout << "T2(int) called with value = " << value << "\n";
+    }
 };
 
 int main() {
-    T1 t1;  // A() called
-    T2 t2;  // A() called
-    T3 t3;
+    T1 t1;     // T1 呼叫 T1()，裡面 a() 和 b(4)
+    T2 t2;     // 呼叫 T2() 無參數建構子
+    T2 t3(10); // 呼叫 T2(int) 傳入參數 10
     return 0;
 }
